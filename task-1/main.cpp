e7785f1988afd0f097fbf27bddd005322bfdd192
nuo18 <119784955+nuo18@users.noreply.github.com>
2023-10-06 12:37:02 +0100

    Initial commit

---
+++ task-1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    //Add code here to print out an ASCII art game header
+    //of your choice. Characters should be aligned correctly
+    //such that when the application is run, it prints out a legible
+    //ascii art game header.
+    //---
+    // Some useful resources:
+    //- https://patorjk.com/software/taag/
+    //- https://www.asciiart.eu/
+
+    cout << " ____                          _____ _                        _ \n"
+            "|  _ \\  ___   ___  _ __ ___   | ____| |_ ___ _ __ _ __   __ _| |\n"
+            "| | | |/ _ \\ / _ \\| '_ ` _ \\  |  _| | __/ _ \\ '__| '_ \\ / _` | |\n"
+            "| |_| | (_) | (_) | | | | | | | |___| ||  __/ |  | | | | (_| | |\n"
+            "|____/ \\___/ \\___/|_| |_| |_| |_____|\\__\\___|_|  |_| |_|\\__,_|_|\n";
+
+    //Game name: Doom Eternal
+}
---
+++ task-2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void main()
+{
+    //Add code to prompt the player for their name, and then
+    //greet them with that name. For example, 
+    //
+    // Please enter your name: ben
+    // Hello ben!
+    //
+
+    //Expand this so the player is asked for their name, clan tag,
+    //age and username. Print out these details like so:
+    //
+    // - Name: <name>
+    // - Username: [<clantag>]<username>
+    // - Age: <age>
+    //
+    
+}
