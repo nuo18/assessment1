1672211d1c00fd24b207abb62311934c4570a2c3
nuo18 <119784955+nuo18@users.noreply.github.com>
2023-10-13 12:03:39 +0100

    Task7 done

--- task-7/main.cpp
+++ task-7/main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+using namespace std;
+
+// Declaring a struct
+struct player_details {
+    string name;
+    string username;
+    int age;
+    string playerclass;
+    string sec_weapon;
+    string potion;
+};
+
+void padding();
 
 void main()
 {
@@ -30,4 +43,66 @@ void main()
     // Player details:
     // - Name: ben
     // - Class: Knight
+
+    // Creating a player details struct
+    player_details player1;
+    int player_choice;
+    int break_case = -1;
+
+    cout << "Please choose a character class below: " << endl;
+    // Printing class
+    cout << "1. Spearman\n";
+    cout << "2. Knight\n";
+    cout << "3. Archer";
+    // cout << "4. Wizard\n";......
+
+    // Padding for player input
+    padding();
+    cout << ">  ";
+
+    while (break_case < 0)
+    {
+        // User input
+        cin >> player_choice;
+
+        switch (player_choice) {
+        case 1:
+            player1.playerclass = "Spearman";
+            cout << "-  You have selected the Spearman character class.";
+            padding();
+            break_case = 1;
+            break;
+        case 2:
+            player1.playerclass = "Knight";
+            cout << "-  You have selected the Knight character class.";
+            padding();
+            break_case = 1;
+            break;
+        case 3:
+            player1.playerclass = "Archer";
+            cout << "-  You have selected the Archer character class.";
+            padding();
+            break_case = 1;
+            break;
+        default:
+            cout << "-  Invalid choice!\n-  Please enter your choice again.";
+            padding();
+            continue;
+        }
+    }
+
+    // Asking for username
+    cout << "Please enter your name: ";
+    cin >> player1.username;
+    cout << "\n";
+
+    // Printing player details
+    cout << "Player details: " << endl;
+    cout << "- Name: " << player1.username << endl;
+    cout << "- Class: " << player1.playerclass << endl;
+}
+
+void padding()
+{
+    cout << "\n" << endl;
 }
